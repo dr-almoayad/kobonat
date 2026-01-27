038061de8a01fec87d5925b59cc7dbfcf249a3ea
dr-almoayad <111410639+dr-almoayad@users.noreply.github.com>
2026-01-27 14:12:40 +0200

    Update layout.js

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -165,14 +165,13 @@ export default async function LocaleLayout({ children, params }) {
         />
 
             <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EFNHSXWE0M"></script>
-<script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EFNHSXWE0M"/>
+<script
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-EFNHSXWE0M');
-</script>
+/>
         
         {/* Schema.org for organization */}
         <script
