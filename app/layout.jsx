01ce460a2cd9eec176a5c683fbe7e5017e135895
dr-almoayad <111410639+dr-almoayad@users.noreply.github.com>
2026-02-25 09:57:46 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,23 @@
 // app/layout.jsx
+
+export const metadata = {
+  metadataBase: new URL('https://cobonat.me'),
+  openGraph: {
+    siteName: 'Cobonat',
+  },
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr">
       <body>
-        {/* This is where your app content (including Admin) will be injected */}
         {children}
       </body>
     </html>
