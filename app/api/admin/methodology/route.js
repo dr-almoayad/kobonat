149b57ed2fbf0f0f5bf68f4f2964a1aa27972028
dr-almoayad <111410639+dr-almoayad@users.noreply.github.com>
2026-02-28 06:13:51 +0200

    Update route.js

--- app/api/admin/methodology/route.js
+++ app/api/admin/methodology/route.js
@@ -5,7 +5,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function GET(request) {
    const session = await getServerSession(authOptions);
