3f71f3704b97a14bc48a4c2b605e236fbd754c73
dr-almoayad <111410639+dr-almoayad@users.noreply.github.com>
2026-06-27 00:21:29 +0300

    Update route.js

--- app/api/admin/banks/[id]/route.js
+++ app/api/admin/banks/[id]/route.js
@@ -1,7 +1,7 @@
 // app/api/admin/banks/[id]/route.js
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-import { getServerSession } from 'next-auth';
+import { getServerSession } from 'next-auth'; 
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 async function auth() {
