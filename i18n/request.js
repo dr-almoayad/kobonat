bc77f6558609764c4f0db90189894b73b51d1591
dr-almoayad <111410639+dr-almoayad@users.noreply.github.com>
2026-07-12 07:06:47 +0300

    Fix formatting in request.js for locale assignment

--- i18n/request.js
+++ i18n/request.js
@@ -3,7 +3,7 @@ import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
+  let locale = await requestLocale; 
   
   // Validate locale
   if (!locale || !routing.locales.includes(locale)) {
